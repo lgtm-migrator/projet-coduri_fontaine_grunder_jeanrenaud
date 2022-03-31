2535ecc423455ae3464d66cb088db5e288068316
Luca Coduri <coduri.vitor@gmail.com>
2022-03-31 14:26:07 +0200

    Added simple test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void justAnExample() {
+        assertEquals("Hello JUnit 5", "Hello JUnit 5");
+    }
+}
