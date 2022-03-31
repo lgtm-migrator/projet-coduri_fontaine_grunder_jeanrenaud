8b11087bddc9e3873b65402ceb5316b8450e5288
chloefont <cfontaine@citycable.ch>
2022-03-31 12:08:26 +0200

    implémentation de la commande clean

--- src/main/java/CleanCommand.java
+++ src/main/java/CleanCommand.java
@@ -1,11 +1,25 @@
-import picocli.CommandLine.Command;
+import picocli.CommandLine;
 
-@Command (
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import org.apache.commons.io.FileUtils;
+
+@CommandLine.Command (
         name = "clean"
 )
 public class CleanCommand implements Runnable {
+    @CommandLine.Parameters
+    private Path directoryName;
+
     @Override
     public void run() {
-        System.out.println("clean");
+        File buildDir = new File(directoryName + "/build/");
+        try {
+            FileUtils.deleteDirectory(buildDir);
+        } catch (IOException e){
+            System.out.println("Error occured : " + e.getMessage());
+        }
+
     }
 }
