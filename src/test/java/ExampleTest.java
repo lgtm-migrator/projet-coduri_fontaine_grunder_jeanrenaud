19e3867267db3c81a8f71a0a0951fc2000dd58e9
Luca Coduri <coduri.vitor@gmail.com>
2022-04-01 13:50:31 +0200

    Linter test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class ExampleTest {
     @Test
     public void justAnExample() {
+        int test = 0;
         assertEquals("Hello JUnit 5", "Hello JUnit 5");
     }
 }
