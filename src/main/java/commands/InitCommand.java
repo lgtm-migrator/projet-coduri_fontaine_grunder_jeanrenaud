3aeed5fa928b8000504d68dbdee4c020f5b70f6d
Grunder <yannick.grunder@heig-vd.ch>
2022-04-06 22:03:57 +0200

    Init done and config.yaml added

--- src/main/java/commands/InitCommand.java
+++ src/main/java/commands/InitCommand.java
@@ -1,13 +1,45 @@
 package commands;
 
+import org.codehaus.plexus.util.FileUtils;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Command (  name = "init", mixinStandardHelpOptions = true, version = "init 1.0",
             description = "Initialize a static website in the designed folder." )
 public class InitCommand implements Runnable {
+
+    @Parameters(index = "0")
+    private Path path;
+
+    private File indexSource  = new File("https://github.com/dil-classroom/projet-coduri_fontaine_grunder_jeanrenaud/blob/2b9283a57c808629f8b6cf10b5074b25df192a3d/init/index.md");
+    private File configSource = new File("https://github.com/dil-classroom/projet-coduri_fontaine_grunder_jeanrenaud/blob/2b9283a57c808629f8b6cf10b5074b25df192a3d/init/config.yaml");
+
+
     @Override
     public void run() {
-        System.out.println("init");
+        try {
+
+            Path indexDestPath  = Path.of(path.toString() + "\\index.md");
+            Path configDestPath = Path.of(path+"\\config.yaml");
+
+            File indexDest  = new File(path+"\\index.md");
+            File configDest = new File(path+"\\config.yaml");
+
+            Files.createDirectories(path);
+            Files.createFile(indexDestPath);
+            Files.createFile(configDestPath);
+            FileUtils.copyDirectory(indexSource,indexDest);
+            FileUtils.copyDirectory(configSource,configDest);
+            System.out.println("init donne");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
