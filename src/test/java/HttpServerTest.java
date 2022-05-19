1d473c3e27f7e67fb5e6e2cb0af9e61b510ed80e
chloefont <cfontaine@citycable.ch>
2022-05-19 12:28:08 +0200

    tests ok

--- src/test/java/HttpServerTest.java
+++ src/test/java/HttpServerTest.java
@@ -1,11 +1,12 @@
 import httpServer.SimpleHttpServer;
 import org.junit.After;
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
-import static org.junit.Assert.*;
+import static org.apache.commons.io.FileUtils.deleteDirectory;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,9 +14,19 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class HttpServer {
+public class HttpServerTest {
     static final String FOLDER_NAME = "init/mysite/";
     static final String FILE_NAME = "index.html";
+
+    static boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
 
     @BeforeAll
     public static void setUp() {
@@ -42,9 +53,10 @@ public class HttpServer {
 
     }
 
-    @After
+    @AfterAll
     public static void deleteFiles() {
-
+        if (!deleteDirectory(new File(FOLDER_NAME)))
+            System.out.println("Error while deleting folder");
     }
 
     @Test
@@ -101,7 +113,49 @@ public class HttpServer {
 
     @Test
     public void accessRightURLInSubFolder() {
+        String newFile = FOLDER_NAME + "test/" + FILE_NAME;
 
+        try {
+
+            File file = new File(FOLDER_NAME + "test/");
+
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+
+            File indexFile = new File(newFile);
+            if (!indexFile.exists()) {
+                FileWriter writer = new FileWriter(indexFile);
+                writer.write("coucou\n");
+                writer.flush();
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error while creating file : " + e.getMessage());
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SimpleHttpServer server = new SimpleHttpServer(8080, FOLDER_NAME);
+                assertDoesNotThrow(server::start);
+            }
+        });
+        thread.start();
+
+        URL url = assertDoesNotThrow(() -> {
+            return new URL("http://localhost:8080/test");
+        });
+
+        HttpURLConnection connection = assertDoesNotThrow(() -> {
+            return (HttpURLConnection) url.openConnection();
+        });
+
+        int responseCode = assertDoesNotThrow(connection::getResponseCode);
+
+        assertEquals(HttpURLConnection.HTTP_OK, responseCode);
+
+        thread.interrupt();
     }
 
 }
